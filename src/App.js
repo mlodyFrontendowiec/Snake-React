0aeaabb1fde8e3c5a2a49f7ead01a45026f12ff5
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-01-12 19:12:27 +0100

    New features -> grow snake and randomizing food place

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const [direction, setDirection] = useState("RIGHT");
 
   const [food, setFood] = useState(getRandomCordiantions());
-  const [speed, setSpeed] = useState(200);
+  const [speed, setSpeed] = useState(100);
   const [snake, setSnake] = useState([
     [0, 0],
     [2, 0],
@@ -24,8 +24,12 @@ function App() {
     document.addEventListener("keydown", (e) => {
       onKeyDown(e);
     });
+    checkFood();
     const interval = setInterval(moveSnake, speed);
-    return () => clearInterval(interval);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, [snake]);
   const onKeyDown = (e) => {
     switch (e.keyCode) {
@@ -47,9 +51,12 @@ function App() {
   };
 
   const moveSnake = () => {
-    let dots = [...snake];
+    if (checkIfOutofBorders()) {
+      return;
+    }
+    checkFood();
 
-    console.log(dots);
+    let dots = [...snake];
 
     let head = dots[dots.length - 1];
 
@@ -59,7 +66,6 @@ function App() {
         break;
       case "LEFT":
         head = [head[0] - 2, head[1]];
-        console.log(head);
         break;
       case "DOWN":
         head = [head[0], head[1] + 2];
@@ -72,6 +78,63 @@ function App() {
     }
     dots.push(head);
     dots.shift();
+
+    setSnake(dots);
+  };
+
+  const checkIfOutofBorders = () => {
+    let head = snake[snake.length - 1];
+    if (head[0] >= 100 || head[1] >= 100 || head[0] < 0 || head[1] < 0) {
+      onGameOver();
+      return true;
+    }
+  };
+  const onGameOver = () => {
+    alert("GAME OVER", "Snake length is " + snake.length);
+    newGame();
+  };
+  const newGame = () => {
+    setDirection("RIGHT");
+    setFood(getRandomCordiantions());
+    setSpeed(200);
+    setSnake([
+      [0, 0],
+      [2, 0],
+    ]);
+  };
+  const checkFood = () => {
+    console.log("Sprawdzam");
+    if (String(snake[snake.length - 1]) === String(food)) {
+      growSnake();
+      setFood(getRandomCordiantions());
+    } else {
+      return;
+    }
+  };
+  const growSnake = () => {
+    let dots = [...snake];
+    console.log(snake);
+
+    let newElement = dots[dots.length - 1];
+
+    switch (direction) {
+      case "RIGHT":
+        newElement = [newElement[0] + 2, newElement[1]];
+        break;
+      case "LEFT":
+        newElement = [newElement[0] - 2, newElement[1]];
+        break;
+      case "DOWN":
+        newElement = [newElement[0], newElement[1] + 2];
+        break;
+      case "UP":
+        newElement = [newElement[0], newElement[1] - 2];
+        break;
+      default:
+        return;
+    }
+    dots.push(newElement);
+    console.log(dots);
 
     setSnake(dots);
   };
